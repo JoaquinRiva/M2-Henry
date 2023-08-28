d817945509ab42ac82a561470169a50a084222e0
Joaquin-Riva <rivajoaquin012@gmail.com>
2023-08-28 16:52:57 -0300

    11_TERMINADO

--- 11-Redux/homework/01 - Exercises/src/index.js
+++ 11-Redux/homework/01 - Exercises/src/index.js
@@ -1,24 +1,54 @@
-const { createStore } = require("redux");
+const {createStore} = require("redux");
 const contador = require("./reducer");
-const { incremento, decremento } = require("./actions");
+const {incremento, decremento} = require("./actions");
 
 // En esta línea creamos nuestro store. Pasándole como parámetro nuestro Reducer
-var store;
+var store = createStore(contador);
 
 // Obtenemos el elemento con el id `valor`.
-var valor;
+var valor = document.getElementById("valor");
 
 // Esta función nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
-// En el primer render y cada vez que nos subscribamos al Store.
+// En el primer render y cada vez que nos subscribamos al Store.// store.subscribe()
 // Utilizamos el elemento obtenido arriba para mostrar el State.
 function renderContador() {
   // Obtenemos la propiedad 'contador' de nuestro store:
   // Seteamos el número obtenido como texto dentro del elemento con id 'valor':
+
+  let counter = store.getState().contador;
+
+  valor.innerHTML = counter;
 }
 
 // Ejecutamos la función 'renderContador':
 
+renderContador();
+
 // Nos subscribimos al store pasándole la misma función. Así cada vez que llegue una acción, ejecutamos la función:
+
+store.subscribe(renderContador);
 
 // Por último, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la acción correspondiente:
+
+let botonIncremento = document.getElementById("incremento");
+let botonDecremento = document.querySelector("#decremento");
+let impar = document.querySelector("#incrementoImpar");
+let incrementoAsync = document.querySelector("#incrementoAsync");
+
+botonIncremento.addEventListener("click", () => {
+  store.dispatch(incremento(1));
+});
+botonDecremento.addEventListener("click", () => {
+  store.dispatch(decremento());
+});
+
+impar.addEventListener("click", () => {
+  store.getState().contador % 2 !== 0 && store.dispatch(incremento(1));
+});
+
+incrementoAsync.addEventListener("click", () => {
+  setTimeout(() => {
+    store.dispatch(incremento(1));
+  }, 2000);
+});
--- 11-Redux/homework/02 - Exercises (vistazo de React)/src/components/Counter.js
+++ 11-Redux/homework/02 - Exercises (vistazo de React)/src/components/Counter.js
@@ -1,15 +1,19 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { increment, decrement } from "../actions";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {increment, decrement} from "../actions";
 
 class Counter extends Component {
   // Extra Credit
   incrementIfOdd = () => {
     //Implementar una función de incremento que sólo aumenta si el valor del contador es impar
+    this.props.count % 2 !== 0 && this.props.increment();
   };
   // Extra Credit
   incrementAsync = () => {
     //  Implementar una función de incremento que aumenta después de esperar un segundo
+    setTimeout(() => {
+      this.props.increment();
+    }, 2000);
   };
 
   render() {
@@ -21,6 +25,7 @@ class Counter extends Component {
         <button
           onClick={() => {
             /* Completar */
+            this.props.increment();
           }}
         >
           + {/* Incremeta */}
@@ -28,17 +33,16 @@ class Counter extends Component {
         <button
           onClick={() => {
             /* Completar */
+            this.props.decrement();
           }}
         >
           - {/* Decrementa */}
         </button>
         {/* Si quieres hacer los extra credit puede descomentar las líneas de abajo */}
-        {/* <button onClick={this.incrementIfOdd}>
-                    incrementa si es impar
-                </button>
-                <button onClick={this.incrementAsync}>
-                    Incrementa después de un segundo
-                </button>  */}
+        <button onClick={this.incrementIfOdd}>incrementa si es impar</button>
+        <button onClick={this.incrementAsync}>
+          Incrementa después de dos segundos
+        </button>
       </p>
     );
   }
@@ -55,8 +59,15 @@ const mapStateToProps = (state) => {
   };
 };
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => {},
+    decrement: () => {},
+  };
+};
+
 // Se llama a la función de connect para que este componente conozca el resto de la arquitectura de redux.
 // Sin esto, este componente es sólo un componente tonto de React.
 //Pasamos todas las funciones que dependen de Redux, junto con el propio componente,
 // para que Redux se dé a conocer a este componente.
-export default connect(mapStateToProps, { increment, decrement })(Counter);
+export default connect(mapStateToProps, {increment, decrement})(Counter);
